79d50077370206c81f26e050a70114e6f3b3c971
OssiB <ossi.bister@gmail.com>
2015-01-08 09:22:31 +0200

    png fixex

--- plot1.R
+++ plot1.R
@@ -2,7 +2,6 @@ setwd("~/Downloads")
 library(sqldf)
 
 filtered <- read.csv.sql("household_power_consumption.txt",sql = "select * from file where Date='1/2/2007" or Date='2/2/2007'"", header = TRUE, sep = ";")
-
-filtered$DateTime <- as.POSIXct(paste(filtered$Date, filtered$Time), format="%d/%m/%Y %H:%M:%S") 
-
-png("R1.png",height = 480,units ="px",width=480)
+png("plot1.png",height = 480,units ="px",width=480)
+hist(filtered$Global_active_power,col="red",main = "Global active power",xlab = "Global active power(kilowatts)",ylim  = c(0,1200))
+dev.off()
